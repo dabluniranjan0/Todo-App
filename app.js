5094cb3fac1800ed9d55185bd894f85dce8c8bf7
Niranjan Kumar <dabluniranjan0@gmail.com>
2023-10-03 18:33:26 +0530

    v1.1

--- app.js
+++ app.js
@@ -8,14 +8,23 @@ let task_container =document.getElementById('task_container');
 
 
 todocard=(del)=>{ 
+        // console.log(del)
 
-     if(del.target.nodeName=='DIV'){
-        let dotoitem = del.target.parentElement;
+}
+
+delcall=(del)=>{
+    // console.log(del);
+    
+}
+svgdel=(del)=>{
+    
+    if(del.target.nodeName=='svg'){
+        let dotoitem = del.target.parentElement.parentElement;
         dotoitem.remove();
         
         console.log("deleted");
        }
-
+   
 }
 cont=(x)=>{
 
@@ -82,6 +91,19 @@ function renderTodo(){
 
     let del =document.createElement('div');
     del.classList.add('delete');
+    del.addEventListener('click',delcall)
+
+    
+
+    //creating delete svg
+    let del_svg=document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    del_svg.setAttribute("viewBox","0 0 16 16");
+    del_svg.setAttribute("width","18");
+    del_svg.setAttribute("fill","white")
+    del_svg.innerHTML='<path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/><path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>';
+
+    del.appendChild(del_svg)
+    del_svg.addEventListener('click',svgdel)
 
     task_container.appendChild(taskEdit);
     taskEdit.appendChild(container);
@@ -94,7 +116,6 @@ function renderTodo(){
 
     })
     
-
 }
 
 function add(){
